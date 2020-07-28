b907a1c8ff0cb885b4d2b3b852ffa9652d19713d
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-07-27 19:59:35 -0500

    Se agrega dependencia de ayuda para testear redux con mocks redux-mock-store y se agregan pruebas unitarias q testean los componente conectados con redux

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, configure, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import store from './store';
+import ProductList from './components/ProductList';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  shallow(<App />)
+ });
+
+// it('renders ul elements list', () => {
+// 	render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   )
+// });
